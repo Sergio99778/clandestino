4d3d9bf2c17aa41a399a78c67866f34b73701605
sergio99778 <sergio4brandon@gmail.com>
2021-03-04 20:30:42 -0600

    Adding form in banner 1 begin.

---
+++ js/inicio__form.js
@@ -0,0 +1,34 @@
+var iteracion = 0;
+var form = document.getElementById('formulario__input');
+function FbotonOn() {
+    var texto = document.getElementById('botonOn');
+    switch (iteracion) {
+        case 0:
+            texto.innerHTML = "Bienvenid@.";
+        break;
+        case 1:
+            document.getElementById("botonOn").innerHTML= "<p>Bienvenid@.</p><span>Este es un espacio </span><span style='color: gold;'>CL4N_D35T1NO.</span>";
+        break;
+        case 2:
+            texto.innerText = "Si quieres acabar con la corrupción y los\n abusos, te puede interesar lo que estamos\n haciendo.";
+        break;
+        case 3:
+            texto.innerText = "¿Cómo quieres que te llamemos?";
+            document.getElementById("formulario__siguiente").style.display = "none";
+            document.getElementById("formulario__datos").style.display = "inline-block"; 
+            document.getElementById('caja').style.display = "inline-block"           
+        break;
+    }
+    iteracion++;
+}
+function escribe() {
+    document.getElementById('formulario__datos').style.display = "none";
+    document.getElementById('botonOn').style.display = "none";
+    escribir = document.getElementById("caja")
+    miTitulo = document.rellenar.titulo.value
+    escribir.innerHTML = "<span>Hola, </span><span style='color: gold;'>"+"@"+ miTitulo +".</span>";
+    }
+window.onload = function() {
+document.rellenar.ver.onclick = escribe
+FbotonOn();
+} 
