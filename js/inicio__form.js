36d689f19fbec5d5af045e3bbc6f6a2944d42184
sergio99778 <sergio4brandon@gmail.com>
2021-03-04 21:15:35 -0600

    Adding form-container in banner1 begin.

--- js/inicio__form.js
+++ js/inicio__form.js
@@ -7,10 +7,10 @@ function FbotonOn() {
             texto.innerHTML = "Bienvenid@.";
         break;
         case 1:
-            document.getElementById("botonOn").innerHTML= "<p>Bienvenid@.</p><span>Este es un espacio </span><span style='color: gold;'>CL4N_D35T1NO.</span>";
+            document.getElementById("botonOn").innerHTML= "<p style = 'width: 60%;'>Bienvenid@.</p><span>Este es un espacio </span><span style='color: gold;'>CL4N_D35T1NO.</span>";
         break;
         case 2:
-            texto.innerText = "Si quieres acabar con la corrupción y los\n abusos, te puede interesar lo que estamos\n haciendo.";
+            texto.innerHTML = "<div style = 'text-align: left;'><span>" + "Si quieres acabar con la corrupción y los\n abusos, te puede interesar lo que estamos\n haciendo.</span></div>";
         break;
         case 3:
             texto.innerText = "¿Cómo quieres que te llamemos?";
